c8a3e91a8a404160928dfeca941af0d4e503d8c4
AsishSamanta1989 <asish.kgec@gmail.com>
2023-11-29 17:12:49 +0530

    added app.jsx

--- admin-client/src/App.jsx
+++ admin-client/src/App.jsx
@@ -1,35 +1,63 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {userState} from "../store/atoms/user.js"
+import { BASE_URL } from './config.js';
+import { useEffect } from 'react';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div style = {
+      {
+        width: "100vw",
+        height : "100vh",
+        backgroundColor: "#C7DFC5"
+      }
+    }>
+       <InitUser />
+      Hi from courseapp
+    </div>
   )
+
+
 }
 
-export default App
+function InitUser(){
+  const setUser = useSetRecoilState(userState);
+
+  const init = async() =>{
+    try{
+      const response = await axios.get(`${BASE_URL}/admin/me`,{
+        header : {
+          "Authorization" : "Bearer " + localStorage.getItem("token")
+        }
+      });
+
+      if (response.data.username){
+        setUser({
+          isLoading : false,
+          userEmail : response.data.username
+        })}else {
+          setUser({
+            isLoading : false,
+            userEmail : null
+        })
+      }
+
+    }catch(e){
+      setUser({
+        isLoading : false,
+        userEmail : null
+
+    })
+  }
+}
+
+useEffect(() =>{
+  init();
+},[]);
+
+return <></>
+
+}
+export default App;
